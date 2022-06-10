77710f547132a5f5268c5defba815f7298c00321
Om Patel <Om.Patel@einfochips.com>
2022-06-10 22:10:13 +0530

    beutify code

--- JsonFIle/func.c
+++ JsonFIle/func.c
@@ -31,12 +31,23 @@ void printList()
     while (ptr != NULL)
     {
         line();
-        printf("Movie Name: %s\nScreen: %d\n", ptr->movie_name, ptr->screen);
+        printf("Movie Name: %s\n", ptr->movie_name);
+        line();
+        printf("Screen: %d\n", ptr->screen);
         printf("Total Seats: %d\nAvailable Seats: %d\nBooked Seats: %d\n", ptr->seat.total_seat, ptr->seat.avail_seat, ptr->seat.booked_seat);
         // print showsheet
-        for (int index = 0; index < (ptr->seat.total_seat - ptr->seat.booked_seat); index++)
+        
+        int design_count = 0;
+        printf("\n");
+        for (int index = 0; index < ptr->seat.total_seat; index++)
         {
+            design_count++;
             printf("%d ", ptr->seat.show_seat[index]);
+            if (design_count == 10)
+            {
+                printf("\n");
+                design_count = 0;
+            }
         }
         ptr = ptr->next;
         printf("\n");
@@ -44,25 +55,26 @@ void printList()
 }
 
 // insert link at the first location
-void insertFirst(char *movie_name, int screen, int total_seats, int seats_index[],int n_booked)
+void insertFirst(char *movie_name, int screen, int total_seats, int seats_index[], int n_booked)
 {
     // create a link
     struct node *link = (struct node *)malloc(sizeof(struct node));
 
     strcpy(link->movie_name, movie_name);
     link->screen = screen;
-    link->seat.avail_seat = total_seats-n_booked;
+    link->seat.avail_seat = total_seats - n_booked;
     link->seat.total_seat = total_seats;
     link->seat.booked_seat = n_booked;
     link->seat.show_seat = (int *)malloc(sizeof(int) * (total_seats));
 
-    //initialize 0 to all seats
+    // initialize 0 to all seats
     for (int index = 0; index < total_seats; index++)
     {
         link->seat.show_seat[index] = 0;
     }
-    for(int index=0;index<n_booked;index++){
-        link->seat.show_seat[seats_index[index]-1] = 1;
+    for (int index = 0; index < n_booked; index++)
+    {
+        link->seat.show_seat[seats_index[index] - 1] = 1;
     }
     // point it to old first node
     link->next = head;
